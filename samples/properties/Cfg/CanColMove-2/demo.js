41fc57ab4905e0cd0c683b1ab0a942d8f3f0086f
bae seon yeong <bae123@softin.co.kr>
2026-02-20 13:03:16 +0900

    CanColMove 수정

--- samples/properties/Cfg/CanColMove-2/demo.js
+++ samples/properties/Cfg/CanColMove-2/demo.js
@@ -1,61 +1,35 @@
-var ib = ib||{};
-ib = {
+var samplePageObj = samplePageObj||{};
+samplePageObj = {
 //시트 초기화 구문
 'init':{
   //공통기능 설정 부분
   "Cfg": {
     "SearchMode": 0,
     "HeaderMerge": 3,
-    "MessageWidth": 300,
-    CanColMove: 2
-  },
-  "Def": {
-    "Row": {
-      "CanFormula": true
-    }
+    "CanColMove":2, 
   },
   //틀고정 좌측 컬럼 설정
   "LeftCols": [
-    {"Header": ["SEQ", "SEQ"], "Type": "Int","Width": 50,"Align": "Center","Name": "SEQ"}
+    {"Header": ["No","No"],"Type": "Int","Width": 50,"Align": "Center","Name": "SEQ","CanMove": 0,"CanFocus": 0}
   ],
   //중앙(메인) 컬럼 설정
   "Cols": [
-    {"Header": ["TASK ID","TASK ID"],"Name": "taskId","MinWidth": 60,"Type": "Text","Align": "Center","Size": 5,"CanEdit": 1,"Icon": "Check"},
-    {"Header": ["TASK","LEVEL 1"],"Name": "lvl1Cnt","MinWidth": 100,"Type": "Text","Align": "Left"},
-    {"Header": ["TASK","LEVEL 2"],"Name": "lvl2Cnt","MinWidth": 100,"Type": "Text","Align": "Left"},
-    {"Header": ["TASK","LEVEL 3"],"Name": "lvl3Cnt","MinWidth": 100,"Type": "Text","Align": "Left"},
-    {"Header": ["TASK","LEVEL 4"],"Name": "lvl4Cnt","MinWidth": 100,"Type": "Text","Align": "Left"},
-    {"Header": ["TASK","LEVEL 5"],"Name": "lvl5Cnt","MinWidth": 100,"Type": "Text","Align": "Left"},
-    {"Header": ["TASK","LEVEL 6"],"Name": "lvl6Cnt","MinWidth": 100,"Type": "Text","Align": "Left"},
-    {"Header": ["선행 Task","선행 Task"],"Name": "preTaskId","MinWidth": 60,"Type": "Text","Align": "Center","CanEdit": 1},
-    {"Header": ["계획기간","시작일"],"Name": "planStYymmdd","MinWidth": 100,"Type": "Date","Align": "Center","CanEdit": 1},
-    {"Header": ["계획기간","시작시간"],"Name": "planStTm","MinWidth": 60,"Type": "Date","Format": "HH:mm","DataFormat": "HHmmss","EditFormat": "HHmm","Align": "Center","CanEdit": 1},
-    {"Header": ["계획기간","종료일"],"Name": "planEndYymmdd","MinWidth": 100,"Type": "Date","Align": "Center","CanEdit": 1},
-    {"Header": ["계획기간","종료시간"],"Name": "planEndTm","MinWidth": 60,"Type": "Date","Format": "HH:mm","DataFormat": "HHmmss","EditFormat": "HHmm","Align": "Center","CanEdit": 1},
-    {"Header": ["계획기간","소요시간"],"Name": "planReqeTm","MinWidth": 60,"Type": "Text","Align": "Center","Formula": formulaTimeCalc,"Color": "#DDDDDD"},
-    {"Header": ["실행기간","시작일"],"Name": "actnStYymmdd","MinWidth": 100,"Type": "Date","Align": "Center","CanEdit": 1},
-    {"Header": ["실행기간","시작시간"],"Name": "actnStTm","MinWidth": 60,"Type": "Date","Format": "HH:mm","DataFormat": "HHmmss","EditFormat": "HHmm","Align": "Center","CanEdit": 1},
-    {"Header": ["실행기간","종료일"],"Name": "actnEndYymmdd","MinWidth": 100,"Type": "Date","Align": "Center","CanEdit": 1},
-    {"Header": ["실행기간","종료시간"],"Name": "actnEndTm","MinWidth": 60,"Type": "Date","Format": "HH:mm","DataFormat": "HHmmss","EditFormat": "HHmm","Align": "Center","CanEdit": 1},
-    {"Header": ["실행기간","소요시간"],"Name": "actnReqeTm","MinWidth": 60,"Type": "Text","Align": "Center","Formula": formulaTimeCalc,"Color": "#DDDDDD"},
-    {"Header": ["진행상태","진행상태"],"Name": "prgrStaCd","MinWidth": 70,"Type": "Enum","Align": "Center","CanEdit": 1,"EnumKeys": "|01|02|03","Enum": "|미시작|진행중|완료","Button": "Defaults","Icon": ""},
-    {"Header": ["파트","파트"],"Name": "chgrDptNm","MinWidth": 70,"Type": "Text","Align": "Left","CanEdit": 1},
-    {"Header": ["작업자","작업자"],"Name": "perfEmpNm","MinWidth": 70,"Type": "Text","Align": "Center","CanEdit": 1},
-    {"Header": ["확인자","확인자"],"Name": "cnfmEmpNm","MinWidth": 70,"Type": "Text","Align": "Center","CanEdit": 1},
-    {"Header": ["검수자","검수자"],"Name": "inspEmpNm","MinWidth": 70,"Type": "Text","Align": "Center","CanEdit": 1},
-    {"Header": ["비고","비고"],"Name": "etccnt","MinWidth": 120,"Type": "Text","Align": "Left","CanEdit": 1},
-    {"Header": ["차수","차수"],"Name": "ordr","MinWidth": 50,"Type": "Int","Align": "Right","CanEdit": 1},
-    {"Header": ["작업자","작업자"],"Name": "perfEmpCd","MinWidth": 70,"Type": "Text","CanEdit": 0},
-    {"Header": ["확인자","확인자"],"Name": "cnfmEmpCd","MinWidth": 70,"Type": "Text","CanEdit": 0},
-    {"Header": ["검수자","검수자"],"Name": "inspEmpCd","MinWidth": 70,"Type": "Text","CanEdit": 0},
-    {"Header": ["프로젝트 코드","프로젝트 코드"],"Name": "prjCd","MinWidth": 80,"Type": "Text","CanEdit": 0},
-    {"Header": ["TASK ID","TASK ID"],"Name": "orgTaskId","MinWidth": 80,"Type": "Text","CanEdit": 0}
+    {"Header": ["회사명","회사명"],"Type": "Text","Name": "sCorp","Width": "100","Align": "Center","CanEdit": 1},
+    {"Header": ["사원수","사원수"],"Type": "Int","Name": "sPerson","Width": "80","Align": "Right","CanEdit": 1},
+    {"Header": ["년월일","Ymd"],"Name": "sDate_Ymd","Extend": {"Type": "Date","Align": "Center","Width": 110,"Format": "yyyy/MM/dd","DataFormat": "yyyyMMdd","EditFormat": "yyyyMMdd","Size": 8,"EmptyValue": "<span style='color:#AAA'>년,월,일 순으로 숫자만 입력해 주세요.</span>"},"Width": 110},
+    {"Header": ["년월일","Ym"],"Name": "sDate_Ym","Extend": {"Type": "Date","Align": "Center","Width": 80,"Format": "yyyy/MM","DataFormat": "yyyyMM","EditFormat": "yyyyMM","Size": 6,"EmptyValue": "<span style='color:#AAA'>년,월 순으로 숫자만 입력해 주세요.</span>"},"Width": 90},
+    {"Header": ["년월일","Md"],"Name": "sDate_Md","Extend": {"Type": "Date","Align": "Center","Width": 60,"Format": "MM/dd","EditFormat": "MMdd","DataFormat": "MMdd","Size": 4,"EmptyValue": "<span style='color:#AAA'>월,일 순으로 숫자만 입력해 주세요.</span>"},"Width": 90},
+    {"Header": ["시분초","Hms"],"Name": "sDate_Hms","TimePicker": 1,"Extend": {"Type": "Date","Align": "Center","Width": 70,"Format": "HH:mm:ss","EditFormat": "HHmmss","DataFormat": "HHmmss","Size": 8,"EmptyValue": "<span style='color:#AAA'>시,분,초 순으로 8개 숫자만 입력해 주세요.</span>"},"Width": 100},
+    {"Header": ["시분초","Hm"],"Name": "sDate_Hm","TimePicker": 1,"Extend": {"Type": "Date","Align": "Center","Width": 70,"Format": "HH:mm","EditFormat": "HHmm","DataFormat": "HHmm","Size": 6,"EmptyValue": "<span style='color:#AAA'>시,분 순으로 4개 숫자만 입력해 주세요.</span>"},"Width": 90},
+    
   ]
 },
 //시트 이벤트
 'event':{
-    onRenderFirstFinish: function(evt) {
-    	evt.sheet.loadSearchData(ib.data);
+    onRenderFirstFinish:function (evtParam) {
+      //create 함수는 비동기로 동작하므로 생성 직후 동작은 여기서 진행
+      var sheet = evtParam.sheet;
+      sheet.loadSearchData(samplePageObj.data);
     }
 },
 //시트객체 생성
@@ -66,34 +40,13 @@ ib = {
     IBSheet.create({
       id: 'sheet', // 생성할 시트의 id
       el: 'sheetDiv', // 시트를 생성할 Dom 객체 및 id
-      options: options, // 생성될 시트의 속성
+      options: options // 생성될 시트의 속성
     });
-},
+  },
+//화면 기능
+'sampleBtn':function () {
+  },
 //조회 데이터
-'data':[{"chgrDptNm":"전략기획","taskId":"100201","actnEndTm":"190000","ordr":"1","preTaskId":"100200","lvl2Cnt":"222","etccnt":"","lvl1Cnt":"111","lvl3Cnt":"333","planEndYymmdd":"20170804","cnfmEmpNm":"김수현","prjCd":"P9009001","actnEndYymmdd":"20170809","perfEmpNm":"박진우","cnfmEmpCd":"P00002217","inspEmpCd":"200010856","planStYymmdd":"20170804","lvl5Cnt":"555","lvl4Cnt":"444","planEndTm":"163000","perfEmpCd":"P00002223","inspEmpNm":"이정호","planStTm":"120000","orgTaskId":"1","actnStYymmdd":"20170805","lvl6Cnt":"666","prgrStaCd":"01","actnStTm":"130000"},{"chgrDptNm":"실행예산","taskId":"100204","actnEndTm":"170000","ordr":"2","preTaskId":"100200","actnStYymmdd":"20170808","orgTaskId":"2","planStTm":"130000","lvl2Cnt":"2","etccnt":"예산 준비 확인","lvl1Cnt":"a","lvl3Cnt":"3","planEndYymmdd":"20170807","lvl6Cnt":"6","prgrStaCd":"02","actnEndYymmdd":"20170810","prjCd":"P9009001","planStYymmdd":"20170807","lvl5Cnt":"5","actnStTm":"112500","planEndTm":"170000","lvl4Cnt":"4"},{"chgrDptNm":"실행예산","taskId":"100302","actnEndTm":"170000","ordr":"3","preTaskId":"100300","actnStYymmdd":"20170808","orgTaskId":"3","planStTm":"140000","lvl2Cnt":"2","etccnt":"","lvl1Cnt":"a","lvl3Cnt":"3","planEndYymmdd":"20170807","lvl6Cnt":"6","prgrStaCd":"01","actnEndYymmdd":"20170810","prjCd":"P9009001","planStYymmdd":"20170807","lvl5Cnt":"5","actnStTm":"112500","planEndTm":"180000","lvl4Cnt":"4"},{"chgrDptNm":"토목사업본부","taskId":"100205","actnEndTm":"170000","ordr":"4","preTaskId":"100200","actnStYymmdd":"20170808","orgTaskId":"4","planStTm":"150000","lvl2Cnt":"2","etccnt":"","lvl1Cnt":"a","lvl3Cnt":"3","planEndYymmdd":"20170807","lvl6Cnt":"6","prgrStaCd":"03","actnEndYymmdd":"20170810","prjCd":"P9009001","planStYymmdd":"20170807","lvl5Cnt":"5","actnStTm":"112500","planEndTm":"190000","lvl4Cnt":"4"},{"chgrDptNm":"토목사업본부","taskId":"100308","actnEndTm":"171900","ordr":"5","preTaskId":"100300","actnStYymmdd":"20170807","orgTaskId":"5","planStTm":"160000","lvl2Cnt":"2","etccnt":"","lvl1Cnt":"111","lvl3Cnt":"3","planEndYymmdd":"20170807","lvl6Cnt":"","prgrStaCd":"02","actnEndYymmdd":"20170810","prjCd":"P9009001","planStYymmdd":"20170807","lvl5Cnt":"5","actnStTm":"091100","planEndTm":"200000","lvl4Cnt":"4"},{"chgrDptNm":"토목사업본부","taskId":"100210","actnEndTm":"171900","ordr":"6","preTaskId":"100200","actnStYymmdd":"20170807","orgTaskId":"6","planStTm":"170000","lvl2Cnt":"2","etccnt":"","lvl1Cnt":"1","lvl3Cnt":"3","planEndYymmdd":"20170807","lvl6Cnt":"","prgrStaCd":"02","actnEndYymmdd":"20170811","prjCd":"P9009001","planStYymmdd":"20170807","lvl5Cnt":"5","actnStTm":"091100","planEndTm":"210000","lvl4Cnt":"4"},{"chgrDptNm":"안정보건","taskId":"100224","actnEndTm":"171900","ordr":"7","preTaskId":"100200","actnStYymmdd":"20170807","orgTaskId":"7","planStTm":"180000","lvl2Cnt":"2","etccnt":"","lvl1Cnt":"1","lvl3Cnt":"3","planEndYymmdd":"20170807","lvl6Cnt":"6","prgrStaCd":"02","actnEndYymmdd":"20170807","prjCd":"P9009001","planStYymmdd":"20170807","lvl5Cnt":"5","actnStTm":"091100","planEndTm":"220000","lvl4Cnt":"4"},{"chgrDptNm":"토목사업본부","taskId":"200331","actnEndTm":"142200","ordr":"8","preTaskId":"200300","actnStYymmdd":"20170807","orgTaskId":"8","planStTm":"190000","lvl2Cnt":"2","etccnt":"","lvl1Cnt":"1","lvl3Cnt":"3","planEndYymmdd":"20170807","lvl6Cnt":"6","prgrStaCd":"01","actnEndYymmdd":"20170807","prjCd":"P9009001","planStYymmdd":"20170807","lvl5Cnt":"5","actnStTm":"081500","planEndTm":"230000","lvl4Cnt":"4"},{"chgrDptNm":"건축시공","taskId":"200355","actnEndTm":"230000","ordr":"9","preTaskId":"200350","lvl2Cnt":"22","etccnt":"최종보고","lvl1Cnt":"11","lvl3Cnt":"33","planEndYymmdd":"20170808","cnfmEmpNm":"김수현","prjCd":"P9009001","actnEndYymmdd":"20170808","perfEmpNm":"박진우","cnfmEmpCd":"P00002217","inspEmpCd":"200010856","planStYymmdd":"20170807","lvl5Cnt":"55","lvl4Cnt":"44","planEndTm":"000000","perfEmpCd":"P00002223","inspEmpNm":"이정호","planStTm":"200000","orgTaskId":"9","actnStYymmdd":"20170807","lvl6Cnt":"66","prgrStaCd":"03","actnStTm":"071100"},{"chgrDptNm":"건축시공","taskId":"200401","actnEndTm":"180000","ordr":"10","preTaskId":"200400","lvl2Cnt":"1231","etccnt":"회계반영","lvl1Cnt":"12","lvl3Cnt":"31","planEndYymmdd":"20170808","cnfmEmpNm":"김수현","prjCd":"P9009001","actnEndYymmdd":"20170808","cnfmEmpCd":"P00002217","perfEmpNm":"박진우","inspEmpCd":"200010856","planStYymmdd":"20170807","lvl5Cnt":"123","lvl4Cnt":"123","planEndTm":"010000","perfEmpCd":"P00002223","inspEmpNm":"이정호","planStTm":"210000","orgTaskId":"10","actnStYymmdd":"20170807","lvl6Cnt":"2312","prgrStaCd":"01","actnStTm":"101400"},{"chgrDptNm":"건축시공","taskId":"200402","actnEndTm":"121500","ordr":"11","preTaskId":"200400","actnStYymmdd":"20170807","orgTaskId":"14","planStTm":"220000","lvl2Cnt":"12121","etccnt":"","lvl1Cnt":"1231","lvl3Cnt":"121","planEndYymmdd":"20170814","lvl6Cnt":"121","prgrStaCd":"01","actnEndYymmdd":"20170814","prjCd":"P9009001","planStYymmdd":"20170807","lvl5Cnt":"212","actnStTm":"111100","planEndTm":"020000","lvl4Cnt":"123"}]
+'data':[{"sCorp":"삼성전자","sPerson":"11196","sPreYear":"9165","sYear":"7147","sGrow":"0.77985","sPreGrow":"0.9","sPay":"6290.9301","sBonus":"545.2073","sNewPerson":"51","sDate_Ymd":"20130423","sDate_Ym":"201304","sDate_Md":"0423","sDate_Hms":"100320","sDate_Hm":"0930","sDate_YmdHms":"20130423091020","sDate_YmdHm":"201304231200","sDate_yyyyMMdd":1563980400000,"sDate_yyyyMM":"201304","sDate_MMddyyyy":"20130423","sDate_ddMMyyyy":"20130423"},{"sCorp":"SK이노베이션","sPerson":"12950","sPreYear":"9265","sYear":"14418","sGrow":"1.55619","sPreGrow":"1.7","sPay":"9345.0219","sBonus":"670.6712","sNewPerson":"94","sDate_Ymd":"20160701","sDate_Ym":"201607","sDate_Md":"0701","sDate_Hms":"091134","sDate_Hm":"0911","sDate_YmdHms":"20130701091134","sDate_YmdHm":"201507010911","sDate_yyyyMMdd":"20160701","sDate_yyyyMM":"201607","sDate_MMddyyyy":"20160701","sDate_ddMMyyyy":"20120701"},{"sCorp":"한국전력공사","sPerson":"10945","sPreYear":"5988","sYear":"12819","sGrow":"2.14078","sPreGrow":"1.7","sPay":"8041.7922","sBonus":"732.6054","sNewPerson":"58","sDate_Ymd":"20160715","sDate_Ym":"201607","sDate_Md":"0715","sDate_Hms":"140715","sDate_Hm":"1412","sDate_YmdHms":"20160715141215","sDate_YmdHm":"201607151412","sDate_yyyyMMdd":"20160715","sDate_yyyyMM":"201607","sDate_MMddyyyy":"20160715","sDate_ddMMyyyy":"20160715"}]
 }
-ib.create();
-
-// 외부 함수
-function formulaTimeCalc(param) {
-  function lpad(str, padLength, padString) {
-    str += '';
-    while (str.length < padLength) { str = padString + str; }
-
-    return str;
-  }
-  if (param.Col === 'planReqeTm') {
-    var start = new Date(param.Row.planStYymmdd + param.Row.planStTm);
-    var end = new Date(param.Row.planEndYymmdd + param.Row.planEndTm);
-    var second = (end - start) / 1000;
-
-    return lpad(Math.floor(second / 3600), 2, '0') + ':' + lpad(Math.floor((second % 3600) / 60), 2, 0);
-  }
-  if (param.Col === 'actnReqeTm') {
-    var start = new Date(param.Row.actnStYymmdd + param.Row.actnStTm);
-    var end = new Date(param.Row.actnEndYymmdd + param.Row.actnEndTm);
-    var second = (end - start) / 1000;
-
-    return lpad(Math.floor(second / 3600), 2, '0') + ':' + lpad(Math.floor((second % 3600) / 60), 2, 0);
-  }
-}
+samplePageObj.create();
